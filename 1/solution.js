dac83b942c9eeb12bb80a4f13298ba58cd8656fa
Michael Glas <michael.glas93@gmail.com>
2025-12-01 13:52:24 +0100

    first day

---
+++ 1/solution.js
@@ -0,0 +1,33 @@
+import fs from "node:fs";
+
+try {
+  const data = fs.readFileSync("./input.txt", "utf8").toString().split("\n");
+  console.log(data);
+
+  let curr = 50;
+  let zeroCounter = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    let res = 0;
+    let value = parseInt(data[i].substring(1));
+    if (data[i].startsWith("R")) {
+      res = curr + value;
+      if (res % 100 === 0) {
+        zeroCounter++;
+      }
+      curr = res;
+    }
+
+    if (data[i].startsWith("L")) {
+      res = curr - value;
+      if (res % 100 === 0) {
+        zeroCounter++;
+      }
+      curr = res;
+    }
+  }
+
+  console.log(zeroCounter);
+} catch (err) {
+  console.error(err);
+}
